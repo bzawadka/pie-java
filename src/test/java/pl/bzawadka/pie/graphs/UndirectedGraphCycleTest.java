59b344938c215b65ac27db8e8602a935c27e2f39
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-02-15 22:57:14 +0100

    simple cycle check - failing test

--- src/test/java/pl/bzawadka/pie/graphs/UndirectedGraphCycleTest.java
+++ src/test/java/pl/bzawadka/pie/graphs/UndirectedGraphCycleTest.java
@@ -13,26 +13,32 @@ public class UndirectedGraphCycleTest {
     public void testIsCyclic() {
         UndirectedGraphCycle underTest = new UndirectedGraphCycle();
 
-        LinkedList<Integer>[] graph = createAdjacencyList(2);
-        //0 1 0 0
-        graph[0].add(1);
-        graph[0].add(0);
+        LinkedList<Integer>[] graph = createGraph(2, 0, 1, 0, 0);
         assertThat(underTest.isCyclic(graph.length, graph)).isTrue();
 
-        graph = createAdjacencyList(4);
-        //0 1 1 2 2 3
-        graph[0].add(1);
-        graph[1].add(2);
-        graph[2].add(3);
+        graph = createGraph(4, 0, 1, 1, 2, 2, 3);
         assertThat(underTest.isCyclic(graph.length, graph)).isFalse();
+
+        graph = createGraph(4, 0, 1, 1, 2, 2, 3, 3, 0);
+        assertThat(underTest.isCyclic(graph.length, graph)).isTrue();
+    }
+
+    private LinkedList<Integer>[] createGraph(int numberOfVertices, int... edges) {
+        LinkedList<Integer>[] graph = createAdjacencyList(numberOfVertices);
+        for (int i = 0; i < edges.length; i = i + 2) {
+            int vertexFrom = edges[i];
+            int vertexTo = edges[i + 1];
+            graph[vertexFrom].add(vertexTo);
+        }
+        return graph;
     }
 
     private LinkedList<Integer>[] createAdjacencyList(int numberOfVertices) {
-        LinkedList<Integer>[] linkedLists = new LinkedList[numberOfVertices];
+        LinkedList<Integer>[] adjacencyList = new LinkedList[numberOfVertices];
         for (int i = 0; i < numberOfVertices; i++) {
-            linkedLists[i] = new LinkedList<>();
+            adjacencyList[i] = new LinkedList<>();
         }
-        return linkedLists;
+        return adjacencyList;
     }
 
 }
