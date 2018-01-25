82a9ee01e6c26a93f6e6757b2de4ff30a182efdf
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2018-01-25 11:01:22 +0800

    coderpad templateˆ

---
+++ src/main/java/pl/bzawadka/pie/Solution.java
@@ -0,0 +1,19 @@
+package pl.bzawadka.pie;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+import org.junit.*;
+import org.junit.runner.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+// https://coderpad.io/sandboxs
+public class Solution {
+
+    public static void main(String[] args) {
+        JUnitCore.main("Solution");
+    }
+}
